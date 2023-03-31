89256ad24322ec6168853540d80ae7a8fa36577a
lePro7483 <100308543+lePro7483@users.noreply.github.com>
2023-03-31 21:14:32 +0200

    Update FunctionsModule.lua

--- Modules/FunctionsModule.lua
+++ Modules/FunctionsModule.lua
@@ -1,9 +1,9 @@
 local module = {}
 
-function module:AddOutput(settingtable,valuetable)
+function module:AddOutput(settingtable,valuetable,gui)
 	local Command = Instance.new("TextLabel")
 	Command.Name = "Command"
-	Command.Parent = script.Parent.Parent.ScrollingFrame
+	Command.Parent = gui.Frame.ScrollingFrame
 	Command.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 	Command.BackgroundTransparency = 1.000
 	Command.Size = UDim2.new(0, 732, 0, 16)
