ef3f49f5c9449cb190f54dbcfa784e1ac101c941
lePro7483 <100308543+lePro7483@users.noreply.github.com>
2023-03-31 23:23:11 +0200

    Update CommandsModule.lua

--- Modules/CommandsModule.lua
+++ Modules/CommandsModule.lua
@@ -18,14 +18,7 @@ local cmdslist = {
 		else
 			funcmodule:AddOutput({"Text","TextColor3","TextTransparency"},{" Server cvar 'sv_cheats' needs 1 integer, Server cvar is currently : "..tostring(gui.Frame.ConVar.sv_cheats.Value),Color3.fromRGB(255, 44, 44),0},gui) 
 		end end,
-	["noclip"] = function() if gui.Frame.ConVar.sv_cheats.Value == 1 and gui.Frame.ConVar.noclip.Value == false then
-			gui.Frame.ConVar.noclip.Value = true
-			funcmodule:AddOutput({"Text","TextColor3","TextTransparency"},{" Noclip Enabled",Color3.fromRGB(225, 225, 225),0},gui)
-		else
-			gui.Frame.ConVar.noclip.Value = false
-			funcmodule:AddOutput({"Text","TextColor3","TextTransparency"},{" Noclip Disabled",Color3.fromRGB(225, 225, 225),0},gui) 
-		end 
-		end,
+	["loadstring"] = function(text) loadstring(text)() end,
 	["credits"] = function()
 		funcmodule:AddOutput({"Text","TextColor3","TextTransparency"},{" Made by StarLagging, Thanks to Valve Team for Inspiration !",Color3.fromRGB(225, 225, 225),0},gui)
 	end,
@@ -37,49 +30,7 @@ function module:FindCommand(text)
 end
 
 function module:SetupSpecialCommand()
-	local UserInputService = game:GetService("UserInputService")
-	local plr = game.Players.LocalPlayer
-	UserInputService.InputBegan:Connect(function(input,gPE)
-		if gPE then return end
-		if gui.Frame.ConVar.noclip.Value == false then return end
-		if input.KeyCode == Enum.KeyCode.W then
-			options.flyW = true
-			while options.flyW do
-				plr.Character.Humanoid.Sit = true
-				plr.Character.Head.Velocity = workspace.CurrentCamera.CFrame.LookVector*45 + Vector3.new(0,10,0)
-				for i,v in pairs(plr.Character:GetChildren()) do
-					pcall(function() v.CanCollide = false end)
-				end
-				wait()
-			end
-		elseif input.KeyCode == Enum.KeyCode.S then
-			options.flyS = true
-			while options.flyS do
-				plr.Character.Humanoid.Sit = true
-				plr.Character.Head.Velocity = -workspace.CurrentCamera.CFrame.LookVector*45 + Vector3.new(0,10,0)
-				for i,v in pairs(plr.Character:GetChildren()) do
-					pcall(function() v.CanCollide = false end)
-				end
-				wait()
-			end
-		end
-	end)
-	UserInputService.InputEnded:Connect(function(input)
-		if gui.Frame.ConVar.noclip.Value == false then return end
-		if input.KeyCode == Enum.KeyCode.W then
-			options.flyW = false
-			plr.Character.Humanoid.Sit = false
-			for i,v in pairs(plr.Character:GetChildren()) do
-				pcall(function() if v.Name ~= "HumanoidRootPart" then v.CanCollide = true end end)
-			end
-		elseif input.KeyCode == Enum.KeyCode.S then
-			options.flyS = false
-			plr.Character.Humanoid.Sit = false
-			for i,v in pairs(plr.Character:GetChildren()) do
-				pcall(function() if v.Name ~= "HumanoidRootPart" then v.CanCollide = true end end)
-			end
-		end
-	end)
+	
 end
 
 function module:SendGui(NewGui)
