fdf92f4751658f7699f20acabcd1414e375aa4dc
lePro7483 <100308543+lePro7483@users.noreply.github.com>
2023-03-31 22:14:33 +0200

    Update CommandsModule.lua

--- Modules/CommandsModule.lua
+++ Modules/CommandsModule.lua
@@ -1,19 +1,20 @@
 local module = {}
+--local funcmodule = loadstring(game:HttpGet("https://raw.githubusercontent.com/lePro7483/SourceConsoleRoblox/main/Modules/FunctionsModule.lua", true))()
 
 local cmdslist = {
-	["echo"] = function(text) module:AddOutput({"Text","TextColor3","TextTransparency"},{text,Color3.fromRGB(225, 225, 225),0},gui) end,
+	["echo"] = function(text) funcmodule:AddOutput({"Text","TextColor3","TextTransparency"},{" "..text,Color3.fromRGB(225, 225, 225),0},gui) end,
 	["clear"] = function() for i,v in pairs(gui.Frame.ScrollingFrame:GetChildren()) do if v.ClassName == "TextLabel" then v:Destroy() end end end,
 	["quit"] = function() gui:Destroy() end,
 	["sv_cheats"] = function(val)
 		if tonumber(val) then
 			if tonumber(val) > 1 then val = 1 end
 			--game:GetService("ServerScriptService").ConVar.sv_cheats.Value = tonumber(val)
-			module:AddOutput({"Text","TextColor3","TextTransparency"},{"Server cvar 'sv_cheats' changed to "..val,Color3.fromRGB(225, 196, 79),0},gui)
+			funcmodule:AddOutput({"Text","TextColor3","TextTransparency"},{" Server cvar 'sv_cheats' changed to "..val,Color3.fromRGB(225, 196, 79),0},gui)
 		else -- tostring(game:GetService("ServerScriptService").ConVar.sv_cheats.Value)
-			module:AddOutput({"Text","TextColor3","TextTransparency"},{"Server cvar 'sv_cheats' needs 1 integer, Server cvar is currently : nil",Color3.fromRGB(255, 44, 44),0},gui) 
+			funcmodule:AddOutput({"Text","TextColor3","TextTransparency"},{" Server cvar 'sv_cheats' needs 1 integer, Server cvar is currently : nil",Color3.fromRGB(255, 44, 44),0},gui) 
 		end end,
 	["credits"] = function()
-		module:AddOutput({"Text","TextColor3","TextTransparency"},{"Made by StarLagging, Thanks to Valve Team for Inspiration !",Color3.fromRGB(225, 225, 225),0},gui)
+		funcmodule:AddOutput({"Text","TextColor3","TextTransparency"},{" Made by StarLagging, Thanks to Valve Team for Inspiration !",Color3.fromRGB(225, 225, 225),0},gui)
 	end,
 }
 
