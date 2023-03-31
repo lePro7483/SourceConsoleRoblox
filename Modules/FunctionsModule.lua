e16e4e852384fe4dfdf1f4be239e37bdbc85cc75
lePro7483 <100308543+lePro7483@users.noreply.github.com>
2023-03-31 22:19:29 +0200

    Update FunctionsModule.lua

--- Modules/FunctionsModule.lua
+++ Modules/FunctionsModule.lua
@@ -1,20 +1,16 @@
 local module = {}
 
-function module:AddOutput(settingtable,valuetable,gui)
-	-- Bugged Somehow
-	print("pliz")
-	local Command = Instance.new("TextLabel",gui.Frame.ScrollingFrame)
+function module:AddOutput(settingtable,valuetable)
+	local Command = Instance.new("TextLabel",_G.ConsoleGui.Frame.ScrollingFrame)
 	Command.Name = "Command"
 	Command.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 	Command.BackgroundTransparency = 1.000
 	Command.Size = UDim2.new(0, 732, 0, 16)
-	print("test")
 	Command.Font = Enum.Font.Roboto
 	Command.Text = ""
 	Command.TextSize = 12.000
 	Command.TextWrapped = true
 	Command.TextXAlignment = Enum.TextXAlignment.Left
-	print("nobug")
 	for i,v in pairs(settingtable) do
 		Command[v] = valuetable[i]
 	end
