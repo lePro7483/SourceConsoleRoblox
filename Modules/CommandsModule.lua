3b6cb6144068adf6bc04c06305a5149b57ecc6a0
lePro7483 <100308543+lePro7483@users.noreply.github.com>
2023-03-31 22:07:46 +0200

    Update CommandsModule.lua

--- Modules/CommandsModule.lua
+++ Modules/CommandsModule.lua
@@ -1,7 +1,7 @@
 local module = {}
 
 local cmdslist = {
-	["echo"] = function(text) module:AddOutput({"Text","TextColor3","TextTransparency"},{text,Color3.fromRGB(225, 225, 225),0}) end,
+	["echo"] = function(text) module:AddOutput({"Text","TextColor3","TextTransparency"},{text,Color3.fromRGB(225, 225, 225),0},gui) end,
 	["clear"] = function() for i,v in pairs(gui.Frame.ScrollingFrame:GetChildren()) do if v.ClassName == "TextLabel" then v:Destroy() end end end,
 	["quit"] = function() gui:Destroy() end,
 	["sv_cheats"] = function(val)
@@ -10,7 +10,7 @@ local cmdslist = {
 			--game:GetService("ServerScriptService").ConVar.sv_cheats.Value = tonumber(val)
 			module:AddOutput({"Text","TextColor3","TextTransparency"},{"Server cvar 'sv_cheats' changed to "..val,Color3.fromRGB(225, 196, 79),0},gui)
 		else -- tostring(game:GetService("ServerScriptService").ConVar.sv_cheats.Value)
-			module:AddOutput({"Text","TextColor3","TextTransparency"},{"Server cvar 'sv_cheats' needs 1 integer, Server cvar is currently : nil",Color3.fromRGB(255, 44, 44),0}) 
+			module:AddOutput({"Text","TextColor3","TextTransparency"},{"Server cvar 'sv_cheats' needs 1 integer, Server cvar is currently : nil",Color3.fromRGB(255, 44, 44),0},gui) 
 		end end,
 	["credits"] = function()
 		module:AddOutput({"Text","TextColor3","TextTransparency"},{"Made by StarLagging, Thanks to Valve Team for Inspiration !",Color3.fromRGB(225, 225, 225),0},gui)
