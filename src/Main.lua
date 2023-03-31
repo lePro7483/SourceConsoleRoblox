92e065e8f465d01b6481ff387451a8b43f6c125b
lePro7483 <100308543+lePro7483@users.noreply.github.com>
2023-03-31 22:23:30 +0200

    Update Main.lua

--- src/Main.lua
+++ src/Main.lua
@@ -3,16 +3,16 @@ local predictnum = 0
 local guimodule = loadstring(game:HttpGet("https://raw.githubusercontent.com/lePro7483/SourceConsoleRoblox/main/Modules/GuiModule.lua", true))()
 local cmdmodule = loadstring(game:HttpGet("https://raw.githubusercontent.com/lePro7483/SourceConsoleRoblox/main/Modules/CommandsModule.lua", true))()
 local funcmodule = loadstring(game:HttpGet("https://raw.githubusercontent.com/lePro7483/SourceConsoleRoblox/main/Modules/FunctionsModule.lua", true))()
-_G.ConsoleGui = ReturnGui()
-_G.ConsoleGui.Frame.CmdEnter.MouseButton1Click:Connect(function()
+gui = ReturnGui()
+gui.Frame.CmdEnter.MouseButton1Click:Connect(function()
 	task.wait()
-	_G.ConsoleGui.Frame.CmdTxtBox.Text = ""
+	gui.Frame.CmdTxtBox.Text = ""
 end)
-_G.ConsoleGui.Frame.CmdTxtBox.Changed:Connect(function()
+gui.Frame.CmdTxtBox.Changed:Connect(function()
 	cmdtext = gui.Frame.CmdTxtBox.Text
 end)
-_G.ConsoleGui.Frame.CmdEnter.MouseButton1Click:Connect(function()
-	funcmodule:AddOutput({"Text","TextColor3","TextTransparency"},{" ] "..cmdtext,Color3.fromRGB(225, 225, 225),0.4},_G.ConsoleGui)
+gui.Frame.CmdEnter.MouseButton1Click:Connect(function()
+	funcmodule:AddOutput({"Text","TextColor3","TextTransparency"},{" ] "..cmdtext,Color3.fromRGB(225, 225, 225),0.4},gui)
 	local answer = cmdmodule:FindCommand(string.lower(cmdtext))
 	local cmdsplt = string.split(cmdtext," ")
 	if answer ~= nil then
@@ -23,7 +23,7 @@ _G.ConsoleGui.Frame.CmdEnter.MouseButton1Click:Connect(function()
 		end
 		cmdtext = ""
 	else
-		funcmodule:AddOutput({"Text","TextColor3","TextTransparency"},{' Unknown command "'..string.lower(cmdsplt[1])..'"',Color3.fromRGB(225, 225, 225),0},_G.ConsoleGui)
+		funcmodule:AddOutput({"Text","TextColor3","TextTransparency"},{' Unknown command "'..string.lower(cmdsplt[1])..'"',Color3.fromRGB(225, 225, 225),0},gui)
 		cmdtext = ""
 	end
 end)
